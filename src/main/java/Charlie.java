c08048c92469e238dce93171a319c94f53cf361d
MattGoddd <MattGoh2004@gmail.com>
2026-08-21 16:44:00 +0800

    Added TASKS functionality

--- src/main/java/Charlie.java
+++ src/main/java/Charlie.java
@@ -7,6 +7,9 @@ public class Charlie {
     private static final String BOT_NAME = "Charlie";
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
     private static final String INDENT = "    ";
+    private static final String[] TASKS = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         String banner = "  ____ _   _    _    ____  _     ___ _____\n"
                 + " / ___| | | |  / \\  |  _ \\| |   |_ _| ____|\n"
@@ -14,7 +17,7 @@ public class Charlie {
                 + "| |___|  _  |/ ___ \\|  _ <| |___ | || |___\n"
                 + " \\____|_| |_/_/   \\_\\_| \\_\\_____|___|_____|\n";
         intro(banner);
-        echo();
+        readCommand();
     }
 
     private static void horizontalLine() {
@@ -47,7 +50,11 @@ public class Charlie {
         horizontalLine();
     }
 
-    private static void echo() {
+    /**
+     * Reads commands until the user enters {@code bye}.
+     * Other input is stored as a task, while {@code list} displays all stored tasks.
+     */
+    private static void readCommand() {
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
                 String input = scanner.nextLine();
@@ -55,8 +62,15 @@ public class Charlie {
                 if (input.equals("bye")) {
                     outro();
                     break;
+                } else if (input.equals("list")) {
+                    for (int i = 0; i < taskCount; i++) {
+                        printBotLine((i + 1) + ". " + TASKS[i]);
+                    }
+                } else {
+                    TASKS[taskCount] = input;
+                    taskCount++;
+                    printBotLine("added: " + input);
                 }
-                printBotLine(input);
                 horizontalLine();
             }
         }
