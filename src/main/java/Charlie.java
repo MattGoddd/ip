54d88ac7a7037615711a6a52cdb4ca3f6ed381a3
MattGoddd <MattGoh2004@gmail.com>
2026-08-21 15:19:56 +0800

    Added echo command

--- src/main/java/Charlie.java
+++ src/main/java/Charlie.java
@@ -1,33 +1,64 @@
+import java.util.Scanner;
+
 /**
  * Starts the CHARLIE chatbot application.
  */
 public class Charlie {
     private static final String BOT_NAME = "Charlie";
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private static final String INDENT = "    ";
     public static void main(String[] args) {
         String banner = "  ____ _   _    _    ____  _     ___ _____\n"
                 + " / ___| | | |  / \\  |  _ \\| |   |_ _| ____|\n"
                 + "| |   | |_| | / _ \\ | |_) | |    | ||  _|\n"
                 + "| |___|  _  |/ ___ \\|  _ <| |___ | || |___\n"
                 + " \\____|_| |_/_/   \\_\\_| \\_\\_____|___|_____|\n";
-        intro(BOT_NAME, banner);
-        outro();
+        intro(banner);
+        echo();
     }
 
     private static void horizontalLine() {
-        String horizontalLine = "____________________________________________________________";
-        System.out.println(horizontalLine);
+        System.out.println(INDENT + HORIZONTAL_LINE);
     }
 
-    private static void intro(String name, String banner) {
+    private static void printBotLine(String message) {
+        System.out.println(INDENT + message);
+    }
+
+    /**
+     * Prints every row of the banner using the standard bot indentation.
+     */
+    private static void printBanner(String banner) {
+        for (String line : banner.split("\n")) {
+            printBotLine(line);
+        }
+    }
+
+    private static void intro(String banner) {
         horizontalLine();
-        System.out.println(banner);
-        System.out.println("Hello! I'm " + name + "!");
-        System.out.println("What do you want to do today?");
+        printBanner(banner);
+        printBotLine("Hello! I'm " + BOT_NAME + "!");
+        printBotLine("What do you want to do today?");
         horizontalLine();
     }
 
     private static void outro() {
-        System.out.println("Goodbye! See you next time.");
+        printBotLine("Goodbye! See you next time.");
         horizontalLine();
+    }
+
+    private static void echo() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                horizontalLine();
+                if (input.equals("bye")) {
+                    outro();
+                    break;
+                }
+                printBotLine(input);
+                horizontalLine();
+            }
+        }
     }
 }
