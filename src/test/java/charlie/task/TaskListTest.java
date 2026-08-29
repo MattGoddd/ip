018da25a74252a8d001a61c13d99f34ebaa47ba0
MattGoddd <MattGoh2004@gmail.com>
2026-08-29 20:50:49 +0800

    Add JUnit tests for TaskList
    Test the mark and findOnDate methods in TaskList.

--- src/test/java/charlie/task/TaskListTest.java
+++ src/test/java/charlie/task/TaskListTest.java
@@ -1,4 +1,66 @@
 package charlie.task;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TaskListTest {
+    @Test
+    public void mark_changedFromUndoneToDone_returnsDoneTask() {
+        Todo todo = new Todo("sample", false);
+        TaskList taskList = new TaskList(List.of(todo));
+        assertTrue(taskList.mark(0).isDone);
+    }
+
+    @Test
+    public void findOnDate_allTasksMatch_returnsAllTasks() {
+        TaskList taskList = new TaskList();
+        for (int i = 0; i < 10; i++) {
+            taskList.add(new Deadline(
+                    "sample " + i,
+                    false,
+                    LocalDate.of(2026, 1, 1)
+            ));
+        }
+        assertEquals(10,
+                taskList.findOnDate(
+                        LocalDate.of(2026, 1, 1)).size()
+        );
+    }
+
+    @Test
+    public void findOnDate_noTasksMatch_returnsEmptyList() {
+        TaskList taskList = new TaskList();
+        for (int i = 0; i < 10; i++) {
+            taskList.add(new Deadline(
+                    "sample " + i,
+                    false,
+                    LocalDate.of(2026, 1, 1)
+            ));
+        }
+        assertEquals(0,
+                taskList.findOnDate(
+                        LocalDate.of(2025, 1, 1)
+                ).size());
+    }
+
+    @Test
+    public void findOnDate_dateWithinEventRange_returnsEvent() {
+        TaskList taskList = new TaskList(
+                List.of(
+                        new Event("sample",
+                                false,
+                                LocalDateTime.of(2026, 1, 1, 0, 0),
+                                LocalDateTime.of(2026, 1, 10, 0, 0))
+                )
+        );
+        assertEquals(1, taskList.findOnDate(
+                LocalDate.of(2026, 1, 5)
+        ).size());
+    }
 }
