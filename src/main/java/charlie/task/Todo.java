7d551c9f82a87758dce5caa4ba7da7fdd7ccb164
MattGoddd <MattGoh2004@gmail.com>
2026-08-30 16:10:47 +0800

    Add JavaDoc comments to Todo methods
    Document copying, display formatting, and save-file serialization behavior.

--- src/main/java/charlie/task/Todo.java
+++ src/main/java/charlie/task/Todo.java
@@ -15,16 +15,32 @@ public class Todo extends Task {
         super(description, isDone);
     }
 
+    /**
+     * Creates a copy of this todo with the requested completion status.
+     *
+     * @param isDone Completion status for the copy.
+     * @return Copied todo with the requested status.
+     */
     @Override
     public Task copyWithStatus(boolean isDone) {
         return new Todo(description, isDone);
     }
 
+    /**
+     * Returns the display representation of this todo.
+     *
+     * @return Todo type, completion status, and description.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
     }
 
+    /**
+     * Converts this todo into the format used in the save file.
+     *
+     * @return Serialized todo data.
+     */
     @Override
     public String saveFileFormat() {
         String status = isDone ? "Done" : "Not done";
